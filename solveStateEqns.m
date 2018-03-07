ead839bdf8bf8cfc9150be8e5545f90f3c08972d
Xing Wang <wangxing@gatech.edu>
2018-03-06 23:46:47 -0600

    submitted

--- solveStateEqns.m
+++ solveStateEqns.m
@@ -15,51 +15,56 @@ G = [v1'*v1, v1'*v2, v1'*v3, v1'*v4;
      v4'*v1, v4'*v2, v4'*v3, v4'*v4];
 beta = inv(G) * [v1'*x, v2'*x, v3'*x, v4'*x]'
 
-%% Problem 3 Check
+%% Problem 3 Check in Matlab
 A = [2 -1; 
-	3 4]
+    3 4];
 A^4
 -275 * eye(2) + 84 * A
 inv(A)
 eye(2) * 6/11 - A / 11
 
-b0 = 1.4249 - 0.0389i
-b1 = log(7/11) / (2 * sqrt(2) * j)
-
-
+c1 = (log(3+sqrt(2)*j) - log(3-sqrt(2)*j)) / (2*sqrt(2)*j)
+c0 = log(3+sqrt(2)*j) - c1 * (3 + sqrt(2)*j)
+c0 * eye(2) + c1 * A
+logm(A)
 %% Problem 4
-syms t
 A = [0 1 0;
     0 0 1;
     -6 -11 -6]
-[M, S] = eig(A)
-M * expm(S * t) * inv(M)
+[M, s] = eig(A)
+syms t
+M * expm(s * t) * inv(M)
 
 expm(A*t)
 
 %% Problem 5
-syms t
 A = [-1 1 0
     0 -2 1;
-    0 0 -3]
-
-b0 = 3*exp(-t) - 3*exp(-2*t) + exp(-3*t)
-b1 = 5*exp(-t)/2 - 4*exp(-2*t) + 3*exp(-3*t)/2
-b2 = exp(-t)/2 - exp(-2*t) + exp(-3*t)/2
+    0 0 -3];
+syms t
+b0 = 3*exp(-t) - 3*exp(-2*t) + exp(-3*t);
+b1 = 5*exp(-t)/2 - 4*exp(-2*t) + 3*exp(-3*t)/2;
+b2 = exp(-t)/2 - exp(-2*t) + exp(-3*t)/2;
 b0*eye(3) + b1*A + b2 * A^2
 
 %% Problem 6
 syms s
 A = [0 1 0; 0 0 1; -1 -2 -2]
 resolvent = inv(s * eye(3) - A)
-ilaplace(resolvent)
+eAt = ilaplace(resolvent)
 % Check
-syms t
 expm(A * t)
 
 %% Problem 7
-B = [0 0 2]'
-eAt * B * 1
+A = [0 1 0;
+    0 0 1;
+    -1 -2 -2];
+B = [0 0 2]';
+u = 1;
+syms s
+resolvent = inv(s * eye(3) - A);
+eAt = ilaplace(resolvent);
+eAt * B * u
 syms t
 syms tau
 integrand = [ 2*exp(-(t - tau)) - 2*exp(-(t - tau)/2)*(cos((3^(1/2)*(t - tau))/2) - (3^(1/2)*sin((3^(1/2)*(t - tau))/2))/3)
@@ -72,7 +77,9 @@ x0 = [1 0 0]'
 naturalResponse = eAt * x0
 xt = naturalResponse + forcedResponse
 
-T = 0:.01:10;
-U = ones(size(T));
-X0 = [1 0 0];
-sys = ss(A, B, C, D);
+subplot(3, 1, 1);
+fplot(xt(1)); xlim([0, 15]); ylim([0.9, 2.1]); xlabel("t"); ylabel("x_1(t)"); %title("x_1(t)");
+subplot(3, 1, 2);
+fplot(xt(2)); xlim([0, 15]); ylim([-0.5, 0.5]); xlabel("t"); ylabel("x_2(t)");
+subplot(3, 1, 3);
+fplot(xt(3)); xlim([0, 15]); ylim([-0.5, 0.5]); xlabel("t"); ylabel("x_3(t)");
