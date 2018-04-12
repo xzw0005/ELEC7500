a135b3887045598306a019bd14195ef6485fa7fa
Xing Wang <wangxing@gatech.edu>
2018-04-12 05:56:44 -0500

    term project simulation left

--- TermProject.m
+++ TermProject.m
@@ -10,16 +10,18 @@ Arow2 = gradient(f2, [h1, h2])
 syms n1 n2
 a1 = 4
 a2 = 4
-h2 = 1
-h1 = 1 + (k2/k1)^2
 k1 = 1
 k2 = .5
+h2 = 1
+h1 = 1 + (k2/k1)^2
 expand(Arow1)
 Arow1
 Arow2
 
 A = [   -k1/(2*a1*(h1 - h2)^(1/2)), k1/(2*a1*(h1 - h2)^(1/2)) ;
         k1/(2*a2*(h1 - h2)^(1/2)),  -k2/(2*a2*h2^(1/2)) - k1/(2*a2*(h1 - h2)^(1/2))]
+    
+    
 B = [1/4 ; 0]
 C = [1 0]
 
@@ -36,5 +38,19 @@ G = C * inv(s * eye(2) - A) * B
 
 syms k0 k1
 K = [k0 k1]
-p = det(s * eye(2) - (A - B*K))
-simplify(p)
+p5 = det(s * eye(2) - (A - B*K))
+simplify(p5)
+
+simplify((s+2-2*j)*(s+2+2*j))
+
+syms s L1 L2
+L = [L1; L2]
+Mo = A - L*C
+M6 = s * eye(2) - (A - L * C)
+p6 = det(M6)
+simplify(p6)
+
+eqn1 = L1 + 9/16 == 4;
+eqn2 = 5/16 * L1 + L2/4 + 1/64 == 8;
+[a, b] = equationsToMatrix([eqn1, eqn2], [L1, L2])
+linsolve(a, b)
