bcc3344bdebd2d180936d1bb0fd4fdc9b37a9731
Xing Wang <wangxing@gatech.edu>
2018-04-12 04:23:56 -0500

    to merge

--- project4.m
+++ project4.m
@@ -75,6 +75,36 @@ A = [1 0 0 0;
     0  0 3 0;
     0  0 0 -4];
 C = [1 0 1 0];
-desired_eigs = [-1 -2 -3 -4];
-L = place(A', C', desired_eigs)
-L = L'
+syms s
+syms L1
+syms L2
+syms L3
+syms L4
+L = [L1; L2; L3; L4];
+Ao = A - L*C
+s*eye(4) - Ao
+p = det(s*eye(4) - Ao)
+simplify(p)
+expand((s+1)*(s+2)*(s+3)*(s+4))
+
+L = [-4; 0; 12; 0];
+Ao = A - L * C
+eig(Ao)
+
+% The estimation error is $e = x - \hat{x}$, and the error dynamics are given by $\dot{e} = (A - LC)e$. 
+% The gain matrix $L$ is designed to place the eigenvalues of the matrix $A-LC$, so that the error dynamics would be asymptotically stable. 
+% However, since the pair (A, C) is not observable, not all of the eigenvalues could be freely chosen by the choice the of gain matrix $L$. 
+% $A$ has two eigenvalues $-2, -4$ associated with the unobservable subspace, and these two eigenvalues of $A$ will also be eigenvalues of $A-LC$, and cannot be changed by the choice of $L$. 
+% On the other hand, the other two eigenvalues are associated with the observable subspace of $A$, and thus can be freely chosen. 
+
+%desired_eigs = [-1 -3 -2 -4];
+%L = place(A', C', desired_eigs)
+%L = L'
+
+%% Problem 5 (Reduced-Order Observer Design)
+A = [1 0;
+     0 3];
+C = [1 1];
+T = [1 1;
+    1 -1];
+T * A * inv(T)
